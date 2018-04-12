bc25194367b571750b2bbff06a4233098c94df15
ncowboy <latentpickuper@gmail.com>
2018-04-12 14:54:04 +0300

    sdefsd

--- PHPlevel1/Lesson4/functions/main.php
+++ PHPlevel1/Lesson4/functions/main.php
@@ -7,13 +7,13 @@ define('A_TAG_CLOSE', '</a>');
 define('IMG_TAG_OPEN', '<img width="200" class="img-thumbnail" src="');
 define('IMG_TAG_CLOSE', '">');
 
- function galleryRender($path) {
+ function galleryRender($path){
 	if(!is_dir($path)) echo 'Директория не существует';
 	$files = scandir($path);
 	if (isset($files)){
-		for($i = 2; $i < count($files); $i++){
-			$imgUrl = WEBROOT . 'img/' . $files[$i]; 
-			echo A_TAG_OPEN . '>' . IMG_TAG_OPEN . $imgUrl . IMG_TAG_CLOSE . A_TAG_CLOSE;
+	  for($i = 2; $i < count($files); $i++){
+		$imgUrl = WEBROOT . 'img/' . $files[$i]; 
+		  echo A_TAG_OPEN . '>' . IMG_TAG_OPEN . $imgUrl . IMG_TAG_CLOSE . A_TAG_CLOSE;
 		}
 	}
  }
